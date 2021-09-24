fed7b0e656ccf2081ced61167b1ca707f83adf88
Dawsen <1076083782@qq.com>
2021-09-24 13:29:30 +0800

    add dotnet core httpclient project

---
+++ HttpClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new System.Net.Http.HttpClient();
+
+            var content = await client.GetStringAsync("http://localhost:8081/songs");
+
+            Console.WriteLine(content);
+        }
+    }
+}
